8186a5a6be7940184615582535f584523a9d70e3
thanhrossi <thanhrossi.tt@gmail.com>
2018-08-21 17:12:40 +0700

    xcxc

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import noiDung from './noiDung.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      trangthai: 'khoitao'
+    }
+  }
+  
+  componentWillMount() {
+    console.log("will mount");
+  }
+  componentDidMount() {
+    console.log("Did mount");
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("Shout componetn update đã chạy");
+    return true; //this is the missing piece
+    
+  }
+  componentWillUpdate(nextProps, nextState) {
+    console.log("componetn will update đã chạy");
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componetn did update đã chạy");
+  }
+  
+  
+  capNhatState = () =>{
+    this.setState({
+      trangthai: "trang thai duoc update"
+    });
+  }
+  
   render() {
+    console.log("render cxc");
+    console.log(this.state.trangthai);
+    
     return (
       <div className="App">
         
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button className="btn btn-ranger" onClick={() => this.capNhatState()}>
+                Update 
+                <noiDung></noiDung>
+        </button>
       </div>
     );
   }
---
+++ lifecycle/src/noiDung.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class noiDung extends Component {
+    render() {
+        return (
+            <div>
+                <h4>Xin chào</h4>
+            </div>
+        );
+    }
+}
+
+export default noiDung;
