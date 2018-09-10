f75dc824b733f70b2a0f52c99f79b793e6dcb3e4
thanhrossi <thanhrossi.tt@gmail.com>
2018-09-10 16:08:34 +0700

    update

---
+++ react-router/src/Components/Nav.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+                <div className="container">
+                <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i className="fa fa-bars" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#download">Download</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#features">Features</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
+                    </li>
+                    </ul>
+                </div>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Nav;
